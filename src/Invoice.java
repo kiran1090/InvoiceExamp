50a3d655a05afec43a8a7048052c6c6d69220dad
kiran_a <iamkiranashok@gmail.com>
2023-01-18 13:14:34 +0530

    Added geters & setters

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Invoice {
+public class Invoice {
+    public String PartNumber,Description;
+    public int Quantity;
+    public Double Price;
+    public Invoice(String PartNumber,String Description,int Quantity,Double Price)
+    {
+        this.PartNumber=PartNumber;
+        this.Description=Description;
+        this.Quantity=Quantity;
+        this.Price=Price;
+    }
+    public String getPartNumber()
+    {
+        return PartNumber;
+    }
+    public void setPartNumber(String PartNumber)
+    {
+        this.PartNumber = PartNumber;
+    }
+    public String getDescription()
+    {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        this.Description = description;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.Quantity = quantity;
+    }
+
+    public Double getPrice()
+    {
+        return Price;
+    }
+
+    public void setPrice(Double Price) {
+        this.Price = Price;
+    }
 }
---
+++ src/cls.java
@@ -0,0 +1,20 @@
+ /* Create a class called Invoice that a hardware store might use to represent an invoice for
+         an item sold at the store.
+        An Invoice should include four pieces of information as instance variables‐a part number(type String),
+         a part
+        description(type String),a quantity of the item being purchased (type int) and a price per item  (double).
+         Your
+class should have a constructor that initializes the four instance variables. Provide a set and a get method for
+        each instance variable. In addition, provide a method named getInvoice Amount that calculates the invoice
+        amount (i.e., multiplies the quantity by the price per item), then returns the amount as a double value.
+         If the
+        quantity is not positive, it should be set to 0. If the price per item is not positive,
+         it should be set to 0.0. Write a
+        test application named InvoiceTest that demonstrates class Invoice’s capabilities.
+
+
+
+ Write a Java application that allows the user to enter up to 20 integer grades into an array. Stop the loop by
+        typing in ‐1. Your main method should call an Average method that returns the average of the grades.
+         Use the
+        DecimalFormat class to format the average to 2 decimal places */
